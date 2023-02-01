8c925635c284c54d80c12e7349ebb2bdc1d71b01
StefanKostenko <kostenkostephan@gmail.com>
2023-02-01 13:43:02 +0100

    hecho la pagina destinos falta hacerla bonita

--- symfony-proyecto/src/Controller/PageController.php
+++ symfony-proyecto/src/Controller/PageController.php
@@ -14,6 +14,10 @@ class PageController extends AbstractController
     {
         return $this->render('page/index.html.twig', []);
     }
-
     
+    #[Route('/destinos', name: 'destinos')]
+    public function destinos(): Response
+    {
+        return $this->render('page/destinos.html.twig', []);
+    }
 }
